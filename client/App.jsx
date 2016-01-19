57852f39245b928d874863c639ba0344eb046acd
Julius Rajala <juliusrajala@gmail.com>
2016-01-19 19:02:37 +0200

    Added flow-router to project.

--- client/App.jsx
+++ client/App.jsx
@@ -1,3 +1,15 @@
 var App = React.createClass({
+  mixins: [ReactMeteorData],
+  getInitialState(){
+    return ({user: null})
+  },
+  componentDidMount(){
+    console.log("Main app-component mounted.");
 
+  },
+  render(){
+    return(
+
+    );
+  }
 });
